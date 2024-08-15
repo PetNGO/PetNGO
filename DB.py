54fc3dff63dac0d5f57e88097291e2d657f85e4f
anjumustad <anjumustad0504@gmail.com>
2024-08-15 19:18:39 +0530

    added exception handling

--- DB.py
+++ DB.py
@@ -1,39 +1,43 @@
-
-import pymongo 
+import pymongo
 from bson.objectid import ObjectId
 from pymongo.errors import PyMongoError
 
 client = pymongo.MongoClient("mongodb+srv://akhileshpatil12168:48586566@petngo.nwv97.mongodb.net/")
 
-db= client["PetNgo"]
+db = client["PetNgo"]
 pet_collection = db["Pet"]
 user_collection = db["User"]
 
-
 def create_pet(data):
-    data = dict(data)
-    response = pet_collection.insert_one(data)
-    return response.inserted_id
+    try:
+        data = dict(data)
+        response = pet_collection.insert_one(data)
+        return str(response.inserted_id)
+    except PyMongoError:
+        return "something went wrong"
 
 def create_user(data):
-    data = dict(data)
-    response = user_collection.insert_one(data)
-    return response.inserted_id
+    try:
+        data = dict(data)
+        response = user_collection.insert_one(data)
+        return str(response.inserted_id)
+    except PyMongoError:
+        return "something went wrong"
 
 def all():
-    response = pet_collection.find({})
-    data = []
-    for i in response:
-        i["_id"] = str(i["_id"])
-        data.append(i)
-    return data
-
+    try:
+        response = pet_collection.find({})
+        data = []
+        for i in response:
+            i["_id"] = str(i["_id"])
+            data.append(i)
+        return data
+    except PyMongoError:
+        return "something went wrong"
 
 def pet_info(pet_id):
- 
-        print(pet_id)
+    try:
         response = pet_collection.find_one({"_id": ObjectId(pet_id), "isDeleted": False})
-        print(response)
         if response is None:
             return {"error": "Pet not found"}
         
@@ -42,9 +46,16 @@ def pet_info(pet_id):
             response["_id"] = str(response["_id"])
 
         return response
-  
+    except PyMongoError:
+        return "something went wrong"
+
 def update_pet_info(pet_id: str, data: dict):
-        response = pet_collection.find_one_and_update( {"_id": ObjectId(pet_id)}, {"$set": data}, return_document=True)
+    try:
+        response = pet_collection.find_one_and_update(
+            {"_id": ObjectId(pet_id)},
+            {"$set": data},
+            return_document=True
+        )
         if response is None:
             return {"error": "Pet not found"}
         
@@ -52,11 +63,16 @@ def update_pet_info(pet_id: str, data: dict):
         if "_id" in response:
             response["_id"] = str(response["_id"])
 
-        return response   
+        return response
+    except PyMongoError:
+        return "something went wrong"
 
-  
 def delete_pet(pet_id: str):
-        response = pet_collection.find_one_and_update( {"_id": ObjectId(pet_id)}, {"$set": {"isDeleted": True}})
+    try:
+        response = pet_collection.find_one_and_update(
+            {"_id": ObjectId(pet_id)},
+            {"$set": {"isDeleted": True}}
+        )
         if response is None:
             return {"error": "something went wrong"}
         
@@ -64,13 +80,14 @@ def delete_pet(pet_id: str):
         if "_id" in response:
             response["_id"] = str(response["_id"])
 
-        return response   
-    
-    
-#User
+        return response
+    except PyMongoError:
+        return "something went wrong"
 
-def User_info(User_id):
- 
+# User
+
+def User_info(User_id: str):
+    try:
         response = user_collection.find_one({"_id": ObjectId(User_id), "isDeleted": False})
         if response is None:
             return {"error": "User not found"}
@@ -80,9 +97,16 @@ def User_info(User_id):
             response["_id"] = str(response["_id"])
 
         return response
-  
+    except PyMongoError:
+        return "something went wrong"
+
 def update_User_info(user_id: str, data: dict):
-        response = user_collection.find_one_and_update( {"_id": ObjectId(user_id)}, {"$set": data}, return_document=True)
+    try:
+        response = user_collection.find_one_and_update(
+            {"_id": ObjectId(user_id)},
+            {"$set": data},
+            return_document=True
+        )
         if response is None:
             return {"error": "User not found"}
         
@@ -90,11 +114,16 @@ def update_User_info(user_id: str, data: dict):
         if "_id" in response:
             response["_id"] = str(response["_id"])
 
-        return response   
+        return response
+    except PyMongoError:
+        return "something went wrong"
 
-  
 def delete_User(User_id: str):
-        response = user_collection.find_one_and_update( {"_id": ObjectId(User_id)}, {"$set": {"isDeleted": True}})
+    try:
+        response = user_collection.find_one_and_update(
+            {"_id": ObjectId(User_id)},
+            {"$set": {"isDeleted": True}}
+        )
         if response is None:
             return {"error": "something went wrong"}
         
@@ -102,4 +131,6 @@ def delete_User(User_id: str):
         if "_id" in response:
             response["_id"] = str(response["_id"])
 
-        return response  
+        return response
+    except PyMongoError:
+        return "something went wrong"
--- main.py
+++ main.py
@@ -1,60 +1,83 @@
-from fastapi import FastAPI, Body
-
+from fastapi import FastAPI, Body, HTTPException
 import DB
 import model
+
 app = FastAPI()
 
 # @app.get("/")
 # def root():
-#     return{"m":"hello"}
-
+#     return {"m": "hello"}
 
 @app.get("/all/pets")
 def get_pets():
-    data = DB.all()
-    return{"data":data}
-
+    try:
+        data = DB.all()
+        return {"data": data}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
 @app.post("/create-pet")
-def create_pet(data:model.Pet):
-    id  = DB.create_pet(data)
-    return{"id":id}
+def create_pet(data: model.Pet):
+    try:
+        id = DB.create_pet(data)
+        return {"id": id}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
 @app.get("/pet/{petid}")
-def get_pet_info(petid):
-    data = DB.pet_info(petid)
-    return{"petinfo": data}
+def get_pet_info(petid: str):
+    try:
+        data = DB.pet_info(petid)
+        return {"petinfo": data}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
 @app.put("/pet/{petid}/update")
 def update_pet_info(petid: str, data: dict = Body(...)):
-    data = DB.update_pet_info(petid,data)
-    return {"updated_data": data}
+    try:
+        updated_data = DB.update_pet_info(petid, data)
+        return {"updated_data": updated_data}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
 @app.delete("/pet/{petid}")
-def delete_pet(petid: str,):
-    data = DB.delete_pet(petid)
-    return {"data deleted"}
+def delete_pet(petid: str):
+    try:
+        DB.delete_pet(petid)
+        return {"message": "data deleted"}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
-
-
-# USer
+# User
 
 @app.post("/regi")
-def create(data:model.User):
-    id  = DB.create_user(data)
-    return{"id":id}
+def create_user(data: model.User):
+    try:
+        id = DB.create_user(data)
+        return {"id": id}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
 @app.get("/user/{userid}")
-def get_user_info(userid):
-    data = DB.User_info(userid)
-    return{"userinfo": data}
+def get_user_info(userid: str):
+    try:
+        data = DB.User_info(userid)
+        return {"userinfo": data}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
 @app.put("/user/{userid}/update")
 def update_user_info(userid: str, data: dict = Body(...)):
-    data = DB.update_User_info(userid,data)
-    return {"updated_data": data}
+    try:
+        updated_data = DB.update_User_info(userid, data)
+        return {"updated_data": updated_data}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
 
 @app.delete("/user/{userid}")
-def delete_user(userid: str,):
-    data = DB.delete_User(userid)
-    return {"User deleted"}
+def delete_user(userid: str):
+    try:
+        DB.delete_User(userid)
+        return {"message": "User deleted"}
+    except Exception:
+        raise HTTPException(detail="something went wrong")
